a973459ae271b34acd9bb2b4a2e6c63484a0b6a1
SzostekMateusz <m.szostek00@gmail.com>
2024-05-08 20:07:10 +0200

    New assets imported

--- src/imageImport.js
+++ src/imageImport.js
@@ -3,13 +3,15 @@ import golden_billet from '../src/assets/golden_billet.png';
 import settings from '../src/assets/settings.png';
 import currency_change from '../src/assets/currency_change.png'
 import speaker from '../src/assets/speaker.png'
+import speaker_muted from '../src/assets/speaker_muted.png'
 
 const images ={
     coin,
     golden_billet,
     settings,
     currency_change,
-    speaker
+    speaker,
+    speaker_muted
 }
 
 export default images;
