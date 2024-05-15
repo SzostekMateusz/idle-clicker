1c7adb342b3ed60f483450cbe135a812ce5e922a
SzostekMateusz <m.szostek00@gmail.com>
2024-05-15 19:27:49 +0200

    Completed a component that changes the curiosities displayed on the screen

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,23 @@
-import './news.css'
+import React, { useState, useEffect } from "react";
+import "./news.css"; // Zaimportowana klasa CSS dla stylizacji
+import financialNews from "../data/news";
 
 const News = () => {
-    return (<div className='news'><h1>News</h1></div>  );
-}
- 
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % financialNews.length);
+    }, 8000); // Przesuwa co 2 sekundy
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="news-container">
+      <div class="news">{financialNews[index]}</div>
+    </div>
+  );
+};
+
 export default News;
