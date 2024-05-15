b947ad9e2b6d48ff2571338295641e878c208bc1
SzostekMateusz <m.szostek00@gmail.com>
2024-05-15 19:26:10 +0200

    File containing financial curiosities

---
+++ src/data/news.js
@@ -0,0 +1,11 @@
+const financialNews = [
+  "Tylko mniej wiecej 8% swiatowych pieniedzy istnieje fizycznie. Pozostale 92% to tylko ciag cyfr zapisanych na dyskach komputerow.",
+  "Po I Wojnie Swiatowej hiperinflacja w Niemczech byla tak duza, ze bezwartosciowych pieniedzy uzywano jako tapet lub dawano dzieciom jako zabawki.",
+  "Mowi sie, ze pieniedze sa brudne i to prawda. Ponad 90% banknotow nosi na sobie sladowe ilosci kokainy, a wirus grypy może przetrwac na banknocie nawet 17 dni.",
+  "Jesli masz w kieszeni rownowartosc 10$ i nie masz zadnych zobowiazan finansowych, to jestes bogatszy od ¼ amerykanow.",
+  "Pablo Escobar wydawal miesiecznie 2500 USD na zakup gumek recepturek, do pakowania banknotow w paczki po 100 sztuk.",
+  "Statystyki sa nieublagane. Osoby grajace w loteriach pienieznych trzykrotnie czesciej gina w wypadkach komunikacyjnych w drodze do kolektury, niz wygrywaja glowna nagrode.",
+  "Jesli chcesz sie pozbyc slimakow ze swojego ogrodu, wystarczy zakopac w nim kilka monet - w zetknieciu z miedzia i cynkiem uzytym do ich produkcji doznaja one porazenie elektrycznego.",
+];
+
+export default financialNews;
