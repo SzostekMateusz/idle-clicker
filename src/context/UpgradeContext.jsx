0b0041145139dd3bcf43f7821c04cc3c0f7527dc
SzostekMateusz <m.szostek00@gmail.com>
2024-05-24 10:56:29 +0200

    passive income logic moved to separate file

--- src/context/UpgradeContext.jsx
+++ src/context/UpgradeContext.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useContext, useEffect, useRef, useState } from "react";
+import React, { createContext, useContext } from "react";
 import { useMouseClickingUpgrade } from "../hooks/MouseClickingUpgrade";
-import useSound from "../hooks/useSound";
+import { usePassiveIncomeUpgrade } from "../hooks/passiveIncomeUpgrade";
 
 const UpgradeContext = createContext();
 
@@ -19,45 +19,11 @@ export const UpgradeProvider = ({ children }) => {
     addOneUpgradeCost,
   } = useMouseClickingUpgrade();
 
-  // Passive income upgrade
-
-  const upgradeSoundEffect = useSound();
-
-  const [intervalId, setIntervalId] = useState(null);
-  const [passiveIncomeCounter, setPassiveIncomeCounter] = useState(0);
-  const [passiveIncomeLevel, setPassiveIncomeLevel] = useState(0);
-  const [passiveIncomeUpgradeCost, setPassiveIncomeUpgradeCost] = useState(5);
-
-  const passiveIncomeCounterRef = useRef(passiveIncomeCounter);
-
-  useEffect(() => {
-    passiveIncomeCounterRef.current = passiveIncomeCounter;
-  }, [passiveIncomeCounter]);
-
-  useEffect(() => {
-    return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-    };
-  }, [intervalId]);
-
-  const passiveIncomeUpgrade = () => {
-    if (count >= passiveIncomeUpgradeCost) {
-      setCount((prevCount) => prevCount - passiveIncomeUpgradeCost);
-      setPassiveIncomeCounter((prevCounter) => prevCounter + 2);
-      setPassiveIncomeUpgradeCost((prevCost) => prevCost * 2);
-      setPassiveIncomeLevel((prevLevel) => prevLevel + 1);
-      upgradeSoundEffect();
-
-      const newIntervalId = setInterval(() => {
-        setCount((prevCount) => prevCount + passiveIncomeCounterRef.current);
-      }, 2000);
-      setIntervalId(newIntervalId);
-    } else {
-      alert(`Not enough credits. You need ${passiveIncomeUpgradeCost} coins.`);
-    }
-  };
+  const {
+    passiveIncomeUpgrade,
+    passiveIncomeLevel,
+    passiveIncomeUpgradeCost,
+  } = usePassiveIncomeUpgrade(count, setCount);
 
   return (
     <UpgradeContext.Provider
