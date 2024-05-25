f67d37e9d8dbc14f0d1b241f28a65da13c249a02
SzostekMateusz <m.szostek00@gmail.com>
2024-05-25 15:49:52 +0200

    implemented an upgrade price increase funcionality

--- src/hooks/CashIncomeUpgrade.jsx
+++ src/hooks/CashIncomeUpgrade.jsx
@@ -7,13 +7,14 @@ export const CashIncomeUpgrade = (count, setCount) => {
 
   const [cashIncome, setCashIncome] = useState(0)
   const [cashUpgradeLevel, setCashUpgradeLevel] = useState(0)
-  const [cashUpgradeCost, setCashUpgradeCost] = useState(7)
+  const [cashUpgradeCost, setCashUpgradeCost] = useState(500)
 
   const cashUpgrade = () => {
     if(count >= cashUpgradeCost){
       setCount((prevCount) => prevCount - cashUpgradeCost)
       setCashIncome((prevCashIncome) => prevCashIncome + 10)
       setCashUpgradeLevel(prevCashUpgradeLevel => prevCashUpgradeLevel + 1)
+      setCashUpgradeCost((prevCashUpgradeCost) => Math.round(prevCashUpgradeCost * 1.3))
       UpgradeSoundEffect();
     }
     else{
