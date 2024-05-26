ee61c39101ed1411f941bb0fe1103481620db398
SzostekMateusz <m.szostek00@gmail.com>
2024-05-26 16:43:36 +0200

    implemented totaln income section in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import './navbar.css'
+import { useUpgrade } from '../context/UpgradeContext';
 
 const Navbar = () => {
-    return ( <div className='navbar'><h1>Navbar</h1></div> );
+    const {totalIncome,} = useUpgrade();
+
+
+    function totalIncomeFormat(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+
+
+    return ( 
+    <div className='navbar-container'>
+        <div className='income-section'>
+            <p>Total Income: {totalIncomeFormat(totalIncome)} $</p>
+        </div>     
+    </div> );
 }
  
 export default Navbar;
