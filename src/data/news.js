047e9688bdc5c8aef4448d86b3478b34f4e0f030
SzostekMateusz <m.szostek00@gmail.com>
2024-05-31 12:12:10 +0200

    new news

--- src/data/news.js
+++ src/data/news.js
@@ -6,6 +6,10 @@ const financialNews = [
   "Pablo Escobar wydawal miesiecznie 2500 USD na zakup gumek recepturek, do pakowania banknotow w paczki po 100 sztuk.",
   "Statystyki sa nieublagane. Osoby grajace w loteriach pienieznych trzykrotnie czesciej gina w wypadkach komunikacyjnych w drodze do kolektury, niz wygrywaja glowna nagrode.",
   "Jesli chcesz sie pozbyc slimakow ze swojego ogrodu, wystarczy zakopac w nim kilka monet - w zetknieciu z miedzia i cynkiem uzytym do ich produkcji doznaja one porazenie elektrycznego.",
+  "We Wloszech wystepuje najwiekszy problem zwiazany z niesplacaniem kredytow – ich wartosc wynosi 220 mld dolarow. Na kolejnym miejscu znalazla sie Francja – ok. 140 mld dolarow oraz Hiszpania – ok. 130 mld dolarow.",
+  "W Stanach Zjednoczonych wprowadzono przepis, ktory zabrania umieszczania na monetach wizerunku zyjacych prezydentow.",
+  "Do gry Monopoly kazdego roku drukuje sie wiecej pieniedzy niz istnieje tych prawdziwych na swiecie.",
+  "Najczesciej podrabianym banknotem jest 20 USD. Drugi w kolejnosci to nominal 100 USD. W Stanach Zjednoczonych ciagle obowiazuje kara smierci dla pracownikow mennicy Banku Rezerw Federalnych, ktorzy dopuszcza sie falszerstwa."
 ];
 
 export default financialNews;
