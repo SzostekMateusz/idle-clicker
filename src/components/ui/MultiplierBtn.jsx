18af1fb13efe0b056203f2de24c3413f4a73ae4a
SzostekMateusz <m.szostek00@gmail.com>
2024-06-05 23:25:19 +0200

    added styling to multiplier buttons (active color, and click scaling)

--- src/components/ui/MultiplierBtn.jsx
+++ src/components/ui/MultiplierBtn.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './multiplierBtn.css';
 
-const MultiplierBtn = ({ multiplier, onClick }) => {
+const MultiplierBtn = ({ multiplier, onClick, isSelected  }) => {
   return (
     <div className='btn-container'>
-      <button className='btn' onClick={onClick}>X{multiplier}</button>
+      <button
+      className={`btn ${isSelected ? "selected" : ""}`}
+      onClick={onClick}
+    >
+      X{multiplier}
+    </button>
     </div>
   );
 }
