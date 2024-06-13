722eeeadcebba316c48aa987f3d45cbafb5bcc68
SzostekMateusz <m.szostek00@gmail.com>
2024-06-13 15:52:44 +0200

    created raffle upgrade component

---
+++ src/components/ui/RaffleUpgradeBox.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import "./clickupgradebox.css";
+import images from "../../imageImport.js";
+import Swal from "sweetalert2";
+
+const RaffleUpgradeBox = ({ title, upgradePrice, image, onClick, upgradeLevel, upgradeCost, multiplier }) => {
+  const selectedImage = images[image];
+
+  const handleUpgradeClick = () => {
+    onClick(() => {
+      
+    }, () => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `Not enough credits. You need ${upgradeCost} coins.`,
+        confirmButtonText: 'OK'
+      });
+    });
+  };
+
+  return (
+    <div className="click-upgrade-box-container">
+      <div className="click-upgrade-box-left">
+        <img className="click-asset-image" src={selectedImage} alt={title} />
+        <div className="click-upgrade-level">Lvl.{upgradeLevel}</div>
+      </div>
+      <div className="click-upgrade-box-right">
+        <div className="click-upgrade-title">{title}</div>
+        <div className="click-cont-smt">
+        <div className="clicks-per-upgrade">
+        Win:0 / Lose: 0
+        </div>
+        
+          <button className="click-upgrade-button" onClick={handleUpgradeClick}>
+            Upgrade {upgradePrice}$
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RaffleUpgradeBox;
+
+
+//add raffle win/lose counter
