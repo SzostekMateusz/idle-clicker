92a58cc3b1e0138458e49b20b6da6cc404d9193a
SzostekMateusz <m.szostek00@gmail.com>
2024-06-20 13:21:04 +0200

    Added statistic button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,47 @@
-import './navbar.css'
+import React, { useState, useEffect } from 'react';
+import './navbar.css';
 import { useUpgrade } from '../context/UpgradeContext';
+import StatisticsModal from './StatisticModal';
 
 const Navbar = () => {
-    const {totalIncome, totalMoneySpent} = useUpgrade();
+    const { totalIncome, totalMoneySpent } = useUpgrade();
+    const [date, setDate] = useState(new Date());
+    const [showModal, setShowModal] = useState(false);
+    const [startDate] = useState(new Date());
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(prevDate => new Date(prevDate.getTime() + 24 * 60 * 60 * 1000));
+        }, 10 * 1000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     function counterFormat(number) {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     }
 
+    return (
+        <div className='navbar-container'>
+            <div className='income-section'>
+                <h1>Total Income: {counterFormat(totalIncome)} $</h1>
+            </div>
+            <div className='income-section'>
+                <h1>Money Spent: {counterFormat(totalMoneySpent)} $</h1>
+            </div>
+            <div className='date-section'>
+                <h1>{date.toLocaleDateString()}</h1>
+            </div>
+            <button onClick={() => setShowModal(true)}>Show Statistics</button>
+            {showModal && (
+                <StatisticsModal
+                    startDate={startDate}
+                    currentDate={date}
+                    onClose={() => setShowModal(false)}
+                />
+            )}
+        </div>
+    );
+};
 
-    return ( 
-    <div className='navbar-container'>
-        <div className='income-section'>
-            <h1>Total Income: {counterFormat(totalIncome)} $</h1>  
-        </div>
-        <div className='income-section'>
-            <h1>Money Spent: {counterFormat(totalMoneySpent)} $</h1>  
-        </div>
-        
-    </div> );
-}
- 
 export default Navbar;
